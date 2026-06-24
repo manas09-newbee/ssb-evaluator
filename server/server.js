7b9af34208f5355e4fda4cf72c5abff38189ea43
manas09-newbee <fun394151@gmail.com>
2026-06-24 07:51:23 +0530

    support render port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const sessions = require(
   "./modules/interview/sessionStore"
 );
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
